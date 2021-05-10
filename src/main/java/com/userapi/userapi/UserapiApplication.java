9a3572769a4a256a1b4351b670c84e749175e1e2
maxwellamparbeng <maxwell.amparbeng@turntabl.io>
2021-05-10 16:58:58 +0100

    Added save and list code to userRepo

--- src/main/java/com/userapi/userapi/UserapiApplication.java
+++ src/main/java/com/userapi/userapi/UserapiApplication.java
@@ -1,13 +1,25 @@
 package com.userapi.userapi;
-
+import com.userapi.userapi.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserapiApplication {
 
+
+	@Autowired
+	userService userservice;
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserapiApplication.class, args);
+	}
+
+	@Bean
+	public userService userservice(){
+
+		return new userService();
 	}
 
 }
--- src/main/java/com/userapi/userapi/controller/user.java
+++ src/main/java/com/userapi/userapi/controller/user.java
@@ -1,6 +1,8 @@
 package com.userapi.userapi.controller;
+import com.userapi.userapi.model.User;
+import com.userapi.userapi.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/user/api")
 public class user {
 
-
+    @Autowired
+    userService userservice;
     @GetMapping(path="/list")
-    public String display(){
+    public Iterable<User> display(){
 
-      return "money";
+
+
+        var data = userservice.allUsers();
+        System.out.println("This my data coming from database "+data);
+        return data;
     }
 
 
